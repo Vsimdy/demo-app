c9deaf2080c860699fdcee7ca7aeb2e7ad7f9fb1
Chibuzor victor <chibuzorvictor97@gmail.com>
2023-03-11 06:52:50 -0800

    add icons

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import './index.css';
 
-const myFirstElement = <h1>Hello React!</h1>
+function App(){
+    return(
+        <>
+
+            <h1>
+                Hello world
+            </h1>
+<i className=" bi bi-list" id="menu-icon"></i>;
+<i className="bi bi-caret-down-fill"></i>;
+        </>
+    )
+}
 
 const root = createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+root.render(<App/>);
